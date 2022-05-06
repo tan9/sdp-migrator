4d203e19a0d17596e2850ad28fb541dec609601b
Pei-Tang Huang <tangtheone@gmail.com>
2022-05-06 11:48:55 +0800

    Remove managed compiler plugin and try to clean build section.

--- src/index.ts
+++ src/index.ts
@@ -51,15 +51,39 @@ await replaceInFile({
     exit(MIGRATE_ERROR, `更新 parent POM 版本時發生錯誤: ${error}`)
 });
 
-// 修改 build 設定
+// 移除 POM 已經在上游管控或是不需要的 build 設定
 await replaceInFile({
     files: '**/pom.xml',
     from: /^.*<outputDirectory>src\/main\/webapp\/WEB-INF\/classes<\/outputDirectory>.*$\n/m,
     to: '',
 }).then(replaceResults =>
     commitReplacedResults(
-        '移除 build.outputDirectory 設定',
+        '移除 pom.xml 的 build.outputDirectory 設定',
         replaceResults)
 ).catch(error => {
-    exit(MIGRATE_ERROR, `移除 build.outputDirectory 設定時發生錯誤: ${error}`)
+    exit(MIGRATE_ERROR, `移除 pom.xml 的 build.outputDirectory 設定時發生錯誤: ${error}`)
+});
+
+await replaceInFile({
+    files: '**/pom.xml',
+    from: /^\s*?<plugin>.*?<artifactId>maven-compiler-plugin<\/artifactId>.*?<\/plugin>\s*?$\n/ms,
+    to: '',
+}).then(replaceResults =>
+    commitReplacedResults(
+        '移除 pom.xml 的 maven-compiler-plugin 設定',
+        replaceResults)
+).catch(error => {
+    exit(MIGRATE_ERROR, `移除 pom.xml 的 maven-compiler-plugin 設定時發生錯誤: ${error}`)
+});
+
+await replaceInFile({
+    files: '**/pom.xml',
+    from: /^[ \t]*?<build>\s*?<plugins>\s*?<\/plugins>\s*?<\/build>[ \t]*?$\n/ms,
+    to: '',
+}).then(replaceResults =>
+    commitReplacedResults(
+        '移除 pom.xml 中的空 build 設定',
+        replaceResults)
+).catch(error => {
+    exit(MIGRATE_ERROR, `移除 pom.xml 中的空 build 設定時發生錯誤: ${error}`)
 });
